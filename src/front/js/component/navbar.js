392216a11c251b6b49d754d00054bcfbd3d7f8ad
bramag94 <bramag94@hotmail.com>
2021-04-28 02:14:16 +0000

    se agrega navbar y home Fend

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Button, Dropdown, DropdownButton, FormControl } from "react-bootstrap";
 
-export const Navbar = () => {
+export const Navbar1 = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div className="container">
+			<Navbar bg="light" expand="lg">
+				<Navbar.Brand href="#home">
+					<i className="fab fa-instagram fa-2x d-inline-block align-top" width="30" height="30" />
+					React Bootstrap
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Clases</Nav.Link>
+						<Nav.Link href="#link">Contacto</Nav.Link>
+					</Nav>
+					<DropdownButton inline id="dropdown-basic-button" title="Dropdown button">
+						<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+						<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+					</DropdownButton>
+				</Navbar.Collapse>
+			</Navbar>
+			<script src="https://kit.fontawesome.com/3b05315429.js" crossOrigin="anonymous" />
+		</div>
 	);
 };
